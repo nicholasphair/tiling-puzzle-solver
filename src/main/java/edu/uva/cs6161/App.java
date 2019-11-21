a0d8b9375996baee45d7d334838c19150de3310f
Nick Phair <nickphair@gmail.com>
2019-11-21 17:01:06 -0500

    initial commit

---
+++ src/main/java/edu/uva/cs6161/App.java
@@ -0,0 +1,13 @@
+package edu.uva.cs6161;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/edu/uva/cs6161/AppTest.java
@@ -0,0 +1,20 @@
+package edu.uva.cs6161;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
