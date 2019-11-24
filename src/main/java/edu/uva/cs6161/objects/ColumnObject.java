2c67a495c8b778a0e8939d356cef41b71e06eb79
Nick Phair <nickphair@gmail.com>
2019-11-23 21:19:52 -0500

    initial commit of dancing links structures

---
+++ src/main/java/edu/uva/cs6161/objects/ColumnObject.java
@@ -0,0 +1,6 @@
+package edu.uva.cs6161.objects;
+
+public class ColumnObject extends DanceObject {
+    private int S;
+    private String name;
+}
---
+++ src/main/java/edu/uva/cs6161/objects/DataObject.java
@@ -0,0 +1,5 @@
+package edu.uva.cs6161.objects;
+
+public class DataObject extends DanceObject {
+
+}
