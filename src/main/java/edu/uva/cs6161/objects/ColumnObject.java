297cd049d0859e639215be7e8e7b859460297203
Nick Phair <nickphair@gmail.com>
2019-11-23 21:21:35 -0500

    remove unnecessary danceobject class

--- src/main/java/edu/uva/cs6161/objects/ColumnObject.java
+++ src/main/java/edu/uva/cs6161/objects/ColumnObject.java
@@ -1,6 +1,6 @@
 package edu.uva.cs6161.objects;
 
-public class ColumnObject extends DanceObject {
+public class ColumnObject extends DataObject {
     private int S;
     private String name;
 }
--- src/main/java/edu/uva/cs6161/objects/DataObject.java
+++ src/main/java/edu/uva/cs6161/objects/DataObject.java
@@ -1,5 +1,62 @@
 package edu.uva.cs6161.objects;
 
-public class DataObject extends DanceObject {
+public class DataObject {
+    private DataObject L;
+    private DataObject R;
+    private DataObject U;
+    private DataObject D;
+    private DataObject C;
+
+    public DataObject() {
+
+    }
+
+    public DataObject(DataObject L, DataObject R, DataObject U, DataObject D, DataObject C) {
+        this.L = L;
+        this.R = R;
+        this.U = U;
+        this.D = D;
+        this.C = C;
+    }
+
+    public DataObject getL() {
+        return L;
+    }
+
+    public void setL(DataObject l) {
+        L = l;
+    }
+
+    public DataObject getR() {
+        return R;
+    }
+
+    public void setR(DataObject r) {
+        R = r;
+    }
+
+    public DataObject getU() {
+        return U;
+    }
+
+    public void setU(DataObject u) {
+        U = u;
+    }
+
+    public DataObject getD() {
+        return D;
+    }
+
+    public void setD(DataObject d) {
+        D = d;
+    }
+
+    public DataObject getC() {
+        return C;
+    }
+
+    public void setC(DataObject c) {
+        C = c;
+    }
 
 }
