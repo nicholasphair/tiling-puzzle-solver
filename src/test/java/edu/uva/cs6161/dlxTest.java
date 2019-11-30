207e417cebe35e24913cab6f31ba183417f8ff5d
Nick Phair <nickphair@gmail.com>
2019-11-30 16:19:48 -0500

    test cover and uncover

--- src/test/java/edu/uva/cs6161/dlxTest.java
+++ src/test/java/edu/uva/cs6161/dlxTest.java
@@ -1,23 +1,29 @@
 package edu.uva.cs6161;
 
 import edu.uva.cs6161.structures.ColumnObject;
+import edu.uva.cs6161.structures.DataObject;
 import edu.uva.cs6161.structures.QuadLinkedList;
 import org.junit.Test;
+
+import javax.xml.crypto.Data;
 
 import static org.junit.Assert.*;
 
 public class dlxTest {
 
+    public final static int[][] KNUTH_EXAMPLE = {
+            {0, 0, 1, 0, 1, 1, 0},
+            {1, 0, 0, 1, 0, 0, 1},
+            {0, 1, 1, 0, 0, 1, 0},
+            {1, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 1},
+            {0, 0, 0, 1, 1, 0, 1}
+    };
+
+    public final static String[] KNUTH_EXAMPLE_NAMES = { "A", "B", "C", "D", "E", "F", "G"};
+
     @Test
     public void search() {
-        //int[][] matrix = {
-        //        {0, 0, 1, 0, 1, 1, 0},
-        //        {1, 0, 0, 1, 0, 0, 1},
-        //        {0, 1, 1, 0, 0, 1, 0},
-        //        {1, 0, 0, 1, 0, 0, 0},
-        //        {0, 1, 0, 0, 0, 0, 1},
-        //        {0, 0, 0, 1, 1, 0, 1}
-        //};
 
         int[][] matrix = {
                 {0, 0, 1},
@@ -67,5 +73,60 @@ public class dlxTest {
         dlx solver = new dlx(false);
         ColumnObject columnObject = solver.chooseC(quadLinkedList);
         assertEquals("First", columnObject.getName());
+    }
+
+    /**
+     * See Page 7, Figure 3 for a visual representation https://arxiv.org/pdf/cs/0011047.pdf.
+     */
+    @Test
+    public void testCoverColumn() {
+        QuadLinkedList quadLinkedList = new QuadLinkedList(KNUTH_EXAMPLE, KNUTH_EXAMPLE_NAMES);
+
+        ColumnObject columnA = (ColumnObject) quadLinkedList.getRoot().getR();
+        ColumnObject columnD = quadLinkedList.getRoot();
+        while(!(columnD = (ColumnObject) columnD.getR()).getName().equals("D"));
+        ColumnObject columnG = quadLinkedList.getRoot();
+        while(!(columnG= (ColumnObject) columnG.getR()).getName().equals("G"));
+
+        // Before Cover.
+        assertEquals("A", ((ColumnObject) quadLinkedList.getRoot().getR()).getName());
+        assertNotEquals(columnD, columnD.getD().getD());
+        assertNotEquals(columnG, columnG.getD().getD().getD());
+
+        dlx knuthDancer = new dlx(false);
+        knuthDancer.coverColumn(columnA);
+
+        // After Cover.
+        assertEquals("B", ((ColumnObject) quadLinkedList.getRoot().getR()).getName());
+        assertEquals(columnD, columnD.getD().getD());
+        assertEquals(columnG, columnG.getD().getD().getD());
+
+        // For good measure make sure other links haven't changed.
+        assertEquals(columnD.getD(), columnD.getD().getR().getR().getR());
+    }
+
+    @Test
+    public void testUncoverColumn() {
+        QuadLinkedList quadLinkedList = new QuadLinkedList(KNUTH_EXAMPLE, KNUTH_EXAMPLE_NAMES);
+
+        ColumnObject columnA = (ColumnObject) quadLinkedList.getRoot().getR();
+        ColumnObject columnD = quadLinkedList.getRoot();
+        while(!(columnD = (ColumnObject) columnD.getR()).getName().equals("D"));
+        ColumnObject columnG = quadLinkedList.getRoot();
+        while(!(columnG= (ColumnObject) columnG.getR()).getName().equals("G"));
+
+        // Before Cover.
+        assertEquals("A", ((ColumnObject) quadLinkedList.getRoot().getR()).getName());
+        assertNotEquals(columnD, columnD.getD().getD());
+        assertNotEquals(columnG, columnG.getD().getD().getD());
+
+        dlx knuthDancer = new dlx(false);
+        knuthDancer.coverColumn(columnA);
+        knuthDancer.uncoverColumn(columnA);
+
+        // After Cover and Uncover.
+        assertEquals("A", ((ColumnObject) quadLinkedList.getRoot().getR()).getName());
+        assertNotEquals(columnD, columnD.getD().getD());
+        assertNotEquals(columnG, columnG.getD().getD().getD());
     }
 }
